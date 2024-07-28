d6bd3cc20a461e9bbdd5a859f4573e7618376cc2
raxmatjon <raxmatjonhamidov242@gmail.com>
2024-07-28 11:11:19 +0500

    added logger middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RtStrategy } from './strategies/rt';
 import { AtStrategy } from './strategies/at';
 import { JwtModule } from '@nestjs/jwt';
+import { LoggerMiddleware } from 'src/common/middlewares/logger-middlware';
 
 @Module({
-  imports:[JwtModule.register({})],
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService,AtStrategy,RtStrategy]
+  providers: [AuthService, AtStrategy, RtStrategy],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({path:'/auth/local/signin', method: RequestMethod.ALL });
+  }
+}
